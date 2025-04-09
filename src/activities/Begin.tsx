9ee4ba1202895a80be6ce9b0796080703f33c047
srchae <srchae@jdi-global.com>
2025-04-09 18:00:52 +0900

    ⚡️ Feat: 시작하기 페이지 생성

---
+++ src/activities/Begin.tsx
@@ -0,0 +1,42 @@
+import { AppScreen } from "@stackflow/plugin-basic-ui";
+import { ActivityComponentType } from "@stackflow/react";
+
+import GlobalStyle from "../GlobalStyle";
+import { useFlow } from "../stackflow/stackflow";
+import * as S from "../styles/Common.styled";
+import { LoginLink, LoginText } from "../styles/features/Begin.styled";
+
+const BeginActivity: ActivityComponentType = () => {
+  const { push } = useFlow();
+
+  const handleBegin = () => {
+    push("ArticleActivity", {
+      title: "Hello Daangn",
+    });
+  };
+
+  return (
+    <AppScreen>
+      <S.Container>
+        <GlobalStyle />
+        <S.Content>
+          <S.Title>당신 근처의 당근</S.Title>
+          <S.SubText>
+            동네라서 가능한 모든 것
+            <br />
+            지금 내 동네를 선택하고 시작해보세요!
+          </S.SubText>
+        </S.Content>
+
+        <S.Footer>
+          <S.Button onClick={handleBegin}>시작하기</S.Button>
+          <LoginText>
+            이미 계정이 있나요? <LoginLink>로그인</LoginLink>
+          </LoginText>
+        </S.Footer>
+      </S.Container>
+    </AppScreen>
+  );
+};
+
+export default BeginActivity;
