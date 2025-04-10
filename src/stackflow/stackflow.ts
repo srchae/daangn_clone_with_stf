788ba44cabb03df741e102f6d020476bc4dfbb6a
srchae <srchae@jdi-global.com>
2025-04-10 17:52:01 +0900

    ⚡️ Feat: 메인 액티비티 생성

--- src/stackflow/stackflow.ts
+++ src/stackflow/stackflow.ts
@@ -2,16 +2,16 @@ import { basicUIPlugin } from "@stackflow/plugin-basic-ui";
 import { basicRendererPlugin } from "@stackflow/plugin-renderer-basic";
 import { stackflow } from "@stackflow/react";
 
-import ArticleActivity from "../activities/Article";
 import BeginActivity from "../activities/Begin";
 import LoginActivity from "../activities/Login";
+import MainActivity from "../activities/Main";
 
 export const { Stack, useFlow } = stackflow({
   transitionDuration: 300,
   activities: {
     BeginActivity,
-    ArticleActivity,
     LoginActivity,
+    MainActivity,
   },
   plugins: [
     basicRendererPlugin(),
